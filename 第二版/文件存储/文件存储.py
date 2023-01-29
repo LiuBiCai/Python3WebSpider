0e32fd64bceef9340b2a07c063c8471332e05a1a
闭才 刘 <liubicai@outlook.com>
2023-01-29 20:14:58 +0800

    mysql and Mongo

---
+++ 第二版/文件存储/文件存储.py
@@ -0,0 +1,8 @@
+﻿import csv
+
+with open('data.csv','a',encoding='utf-8',newline='') as csvfile:
+	fieldnames=['id','name','age']
+	writer=csv.DictWriter(csvfile,fieldnames=fieldnames)
+	
+	writer.writerow({'id':'10004','name':'王伟','age':20})
+	
